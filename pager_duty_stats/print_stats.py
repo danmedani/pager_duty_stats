0fc22484e16927acbf49dcf25e77ae1cad9dd825
Dan Medani <danmedani@gmail.com>
2020-05-17 18:43:42 -0700

    better help on options

--- pager_duty_stats/print_stats.py
+++ pager_duty_stats/print_stats.py
@@ -8,18 +8,24 @@ import argparse
 
 DEFAULT_START_DATE = '2010-01-01'
 
+GROUPING_WINDOW_HELP = '''
+Group alerts by day, or by week? If by week, this only collects complete weeks (from Monday -> Sunday)
+'''
+
 def get_api_key(file_name: str) -> str:
 	with open(file_name, 'r') as file:
 		return file.readlines()[0].rstrip('\n')
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Aggregate PagerDuty Stats & output csv')
+	parser = argparse.ArgumentParser(description='Aggregate PagerDuty stats & output a csv')
+
 	parser.add_argument('--pd-key-file', default='.api_key', help='File containing API Key to access api.pagerduty.com')
 	parser.add_argument('--start-date', default=DEFAULT_START_DATE, help='Date to collect alerts from')
-	parser.add_argument('--end-date', default=str(datetime.now().date()), help='Date to collect alerts until')
+	parser.add_argument('--end-date', default=str(datetime.now().date()), help='Date to collect alerts until (default: today)')
 	parser.add_argument('--service_ids', required=True, type=str, nargs='+', help='PD service ids to collect stats on')
+	parser.add_argument('--grouping-window', default='week', type=GroupingWindow, choices=list(GroupingWindow), help=GROUPING_WINDOW_HELP)
 	parser.add_argument('--max-error-types', type=int, default=10, help='Max number of types to group by')
-	parser.add_argument('--grouping-window', default='week', type=GroupingWindow, choices=list(GroupingWindow))
+	
 	options = parser.parse_args(sys.argv[1:])
 	
 	incidents = fetch_all_incidents(
