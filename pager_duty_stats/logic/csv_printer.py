52ae62196cba7a8d8599dfded338514ded645fee
Dan Medani <danmedani@gmail.com>
2020-05-18 23:02:22 -0700

    Using csv writer

--- pager_duty_stats/logic/csv_printer.py
+++ pager_duty_stats/logic/csv_printer.py
@@ -1,3 +1,5 @@
+import csv
+import sys
 from typing import Dict
 from datetime import datetime
 from datetime import timedelta
@@ -28,54 +30,44 @@ def print_statistics(
 			for error_type, _ in stats[date_str]['error_type_counts'].items():
 				all_incident_types_set.add(error_type)
 
-
 		current_date += timedelta(days=1)
 
 	all_incident_types = list(all_incident_types_set)
 	all_services = list(all_services_set)
 	all_times_of_day = list(all_times_of_day_set)
 
-	header_str = '{}\t{}'.format(
-		date_col,
-		'Total Pages',
-	)
+	fieldnames = [date_col, 'Total Pages']
+
 	for service in all_services:
-		header_str = header_str + '\t' + service
+		fieldnames.append(service)
 	for time_of_day in all_times_of_day:
-		header_str = header_str + '\t' + time_of_day
+		fieldnames.append(time_of_day)
 	for error_type in all_incident_types:
-		header_str = header_str + '\t' + error_type
-	print(header_str)
+		fieldnames.append(error_type)
+
+	writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, delimiter='\t')
+	writer.writeheader()
 
 	earliest_date = get_earlist_date(list(stats.keys()))
 	current_date = datetime.strptime(earliest_date, '%Y-%m-%d')
 	while current_date <= datetime.now():
 		date_str = str(current_date.date())
 		if date_str in stats:
-			row_str = '{}\t{}'.format(
-				date_str,
-				stats[date_str]['total_pages']
-			)
+			row_dict = {
+				date_col: date_str,
+				'Total Pages': stats[date_str]['total_pages'],
+			}
 
 			for service in all_services:
-				if service in stats[date_str]['per_service']:
-					row_str = row_str + '\t' + str(stats[date_str]['per_service'][service])
-				else:
-					row_str = row_str + '\t' + '0'
+				row_dict[service] = str(stats[date_str]['per_service'][service]) if service in stats[date_str]['per_service'] else '0'
 
 			for time_of_day in all_times_of_day:
-				if time_of_day in stats[date_str]['per_time_of_day']:
-					row_str = row_str + '\t' + str(stats[date_str]['per_time_of_day'][time_of_day])
-				else:
-					row_str = row_str + '\t' + '0'
+				row_dict[time_of_day] = str(stats[date_str]['per_time_of_day'][time_of_day]) if time_of_day in stats[date_str]['per_time_of_day'] else '0'
 
 			for error_type in all_incident_types:
-				if error_type in stats[date_str]['error_type_counts']:
-					row_str = row_str + '\t' + str(stats[date_str]['error_type_counts'][error_type])
-				else:
-					row_str = row_str + '\t' + '0'
+				row_dict[error_type] = str(stats[date_str]['error_type_counts'][error_type]) if error_type in stats[date_str]['error_type_counts'] else '0'
 
-			print(row_str)
+			writer.writerow(row_dict)
 		current_date += timedelta(days=1)
 
 
