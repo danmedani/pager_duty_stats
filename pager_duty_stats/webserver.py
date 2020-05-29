19f16b7661a5af16dfad745d7f107b314a800e54
Dan Medani <danmedani@gmail.com>
2020-05-29 11:39:03 -0700

    A front end!

--- pager_duty_stats/webserver.py
+++ pager_duty_stats/webserver.py
@@ -1,7 +1,25 @@
+from flask import request
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='web/')
 
 @app.route('/')
 def index():
-    return "<span style='color:red'>Hi, I am pager duty app</span>"
+    return """
+<h1>Weclome to Pager Duty Stats v0.1</h1>
+
+<form action="/view_graph" method="post">
+  Service ID <input type="text" name="service_id"></input>
+  Start Date <input type="text" name="start_date"></input>
+  Grouping Window <input type="text" name="grouping_window"></input>
+  <input type="submit" value="View Graph"></input>
+</form>
+
+"""
+
+@app.route('/view_graph', methods = ['POST'])
+def view_graph():
+    service_id = request.form['service_id']
+    start_date = request.form['start_date']
+    grouping_window = request.form['grouping_window']
+    return app.send_static_file('chart.html')
